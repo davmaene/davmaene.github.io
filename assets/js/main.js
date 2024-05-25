c57b8ae91a31bf753e0c62096cc95f4c1ef05237
davmaene <davidmened@gmail.com>
2024-05-25 20:12:53 +0200

    --version done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,337 +1,95 @@
-const auther = "David Maene";
-const contactphone = "+243 970 284 772";
-const contactemail = "developer.david.maene@gmail.com";
-const contacthire = "developer.david.maene@gmail.com"
-const twitter = "https://";
-const linkedin = "https://";
-const github = "https://github.com/davmaene";
-const facebook = "https://facebook.com/davidmaene73";
-const addresse = "RDCongo / Nord-Kivu";
-const street = "Goma City, C. Karisimbi, Q. Murara, Av. Uvira N30";
+$(function () {
 
-(function ($) {
-  "use strict"
+    "use strict";
 
-  /* 1. Proloder */
-  $(window).on('load', function () {
-    $('#preloader-active').delay(450).fadeOut('slow');
-    $('body').delay(450).css({
-      'overflow': 'visible'
+    //===== Prealoder
+
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
     });
-  });
-
-  /* 2. sticky And Scroll UP */
-  $(window).on('scroll', function () {
-    var scroll = $(window).scrollTop();
-    if (scroll < 400) {
-      $(".header-sticky").removeClass("sticky-bar");
-      $('#back-top').fadeOut(500);
-    } else {
-      $(".header-sticky").addClass("sticky-bar");
-      $('#back-top').fadeIn(500);
-    }
-  });
-  // Scroll Up
-  $('#back-top a').on("click", function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 800);
-    return false;
-  });
 
 
-  /* 3. slick Nav */
-  // mobile_menu
-  var menu = $('ul#navigation');
-  if (menu.length) {
-    menu.slicknav({
-      prependTo: ".mobile_menu",
-      closedSymbol: '+',
-      openedSymbol: '-'
-    });
-  };
+  //===== Sticky
 
-  /* 4. MainSlider-1 */
-  // h1-hero-active
-  function mainSlider() {
-    var BasicSlider = $('.slider-active');
-    BasicSlider.on('init', function (e, slick) {
-      var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-      doAnimations($firstAnimatingElements);
-    });
-    BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-      var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-      doAnimations($animatingElements);
-    });
-    BasicSlider.slick({
-      autoplay: false,
-      autoplaySpeed: 4000,
-      dots: false,
-      fade: true,
-      arrows: false,
-      prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".header_navbar").removeClass("sticky");
+        } else {
+            $(".header_navbar").addClass("sticky");
         }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
-        }
-      }
-      ]
     });
+    
+    
+    //===== Section Menu Active
 
-    function doAnimations(elements) {
-      var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-      elements.each(function () {
-        var $this = $(this);
-        var $animationDelay = $this.data('delay');
-        var $animationType = 'animated ' + $this.data('animation');
-        $this.css({
-          'animation-delay': $animationDelay,
-          '-webkit-animation-delay': $animationDelay
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
         });
-        $this.addClass($animationType).one(animationEndEvents, function () {
-          $this.removeClass($animationType);
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });    
+
+
+    ///===== Progress Bar
+
+    if ($('.progress_line').length) {
+        $('.progress_line').appear(function () {
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width', percent + '%');
+        }, {
+            accY: 0
         });
-      });
     }
-  }
-  mainSlider();
-
-  // blog-activ
-  $('.blog-active').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    arrows: true,
-    slidesToShow: 3,
-    prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
 
 
-  // Brand Active
-  $('.brand-active').slick({
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    speed: 400,
-    arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 780,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
 
 
-  /* 5. Testimonial Active*/
-  var testimonial = $('.h1-testimonial-active');
-  if (testimonial.length) {
-    testimonial.slick({
-      dots: false,
-      infinite: true,
-      speed: 1000,
-      autoplay: false,
-      arrows: true,
-      prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-            arrow: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-            arrow: false
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-            arrow: false
-          }
+    //===== Back to top
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
         }
-      ]
     });
-  }
 
 
-  /* 6. Nice Selectorp  */
-  var nice_Select = $('select');
-  if (nice_Select.length) {
-    nice_Select.niceSelect();
-  }
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function (event) {
+        event.preventDefault();
 
-  /* 7. data-background */
-  $("[data-background]").each(function () {
-    $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-  });
-
-
-  /* 10. WOW active */
-  new WOW().init();
-
-
-
-  // 11. ---- Mailchimp js --------//  
-  function mailChimp() {
-    $('#mc_embed_signup').find('form').ajaxChimp();
-  }
-  mailChimp();
-
-
-
-  // 12 Pop Up Img
-  var popUp = $('.single_gallery_part, .img-pop-up');
-  if (popUp.length) {
-    popUp.magnificPopup({
-      type: 'image',
-      gallery: {
-        enabled: true
-      }
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
     });
-  }
 
 
-
-
-})(jQuery);
-
-(() => {
-
-  $(document).ready(() => {
-
-    $("#auther").html(auther)
-    $("#touchus").html(contactemail)
-    $("#addresse").html(addresse)
-    $("#street").html(street)
-
-    $("[tag=hire]").attr({
-      href: `mailto:${contacthire}`
-    })
-
-    $("#github").attr(
-      {
-        "href": github,
-        "target": "_blank"
-      }
-    )
-
-  });
-
-  $(".contacts").html(contactemail)
-
-  $("#contactform").on("submit", e => {
-    e.preventDefault();
-  })
-
-})();
+});
